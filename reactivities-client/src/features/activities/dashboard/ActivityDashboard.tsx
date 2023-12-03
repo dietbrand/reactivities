d2eb83abe27549683b6eebfe4a719a14fc5c5854
dietbrand <dietbrand.tollenaere@gmail.com>
2023-12-03 11:19:21 +0100

    End of Section 5

---
+++ reactivities-client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,59 @@
+import { Grid } from 'semantic-ui-react';
+import { Activity } from '../../../app/models/activity';
+import ActivityList from './ActivityList';
+import ActivityDetails from '../details/ActivityDetails';
+import ActivityForm from '../form/ActivityForm';
+
+interface Props {
+  activities: Activity[];
+  selectedActivity: Activity | undefined;
+  selectActivity: (id: string) => void;
+  cancelSelectActivity: () => void;
+  editMode: boolean;
+  openForm: (id: string) => void;
+  closeForm: () => void;
+  manipulate: (activity: Activity) => void;
+  deleteActivity: (id: string) => void;
+}
+
+const ActivityDashboard = ({
+  activities,
+  selectedActivity,
+  selectActivity,
+  cancelSelectActivity,
+  editMode,
+  openForm,
+  closeForm,
+  manipulate,
+  deleteActivity,
+}: Props) => {
+  return (
+    <Grid>
+      <Grid.Column width='10'>
+        <ActivityList
+          activities={activities}
+          selectActivity={selectActivity}
+          editMode={editMode}
+          deleteActivity={deleteActivity}
+        />
+      </Grid.Column>
+      <Grid.Column width='6'>
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            cancelSelectActivity={cancelSelectActivity}
+            openForm={openForm}
+          />
+        )}
+        {editMode && (
+          <ActivityForm
+            closeForm={closeForm}
+            activity={selectedActivity}
+            manipulate={manipulate}
+          />
+        )}
+      </Grid.Column>
+    </Grid>
+  );
+};
+export default ActivityDashboard;
