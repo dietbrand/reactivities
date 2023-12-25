02b98baac74887825f45f7bf01d93b5e54af243b
dietbrand <dietbrand.tollenaere@gmail.com>
2023-12-25 14:56:27 +0100

    Added client side login and registration

--- reactivities-client/src/app/layout/NavBar.tsx
+++ reactivities-client/src/app/layout/NavBar.tsx
@@ -1,7 +1,12 @@
-import { Button, Container, Menu } from 'semantic-ui-react';
-import { NavLink } from 'react-router-dom';
+import { Button, Container, Dropdown, Image, Menu } from 'semantic-ui-react';
+import { Link, NavLink } from 'react-router-dom';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 const NavBar = () => {
+  const {
+    userStore: { user, logout },
+  } = useStore();
   return (
     <Menu inverted fixed='top'>
       <Container>
@@ -20,8 +25,26 @@ const NavBar = () => {
             content='Create Activity'
           />
         </Menu.Item>
+        <Menu.Item position='right'>
+          <Image
+            src={user?.image || '/assets/user.png'}
+            avatar
+            spaced='right'
+          />
+          <Dropdown pointing='top right' text={user?.displayName}>
+            <Dropdown.Menu>
+              <Dropdown.Item
+                as={Link}
+                to={`/profile/${user?.username}`}
+                text='My profile'
+                icon='user'
+              />
+              <Dropdown.Item onClick={logout} text='Logout' icon='power' />
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu.Item>
       </Container>
     </Menu>
   );
 };
-export default NavBar;
+export default observer(NavBar);
