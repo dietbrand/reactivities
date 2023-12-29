f87fb9c8cbb6f4b7fd4a23fc23212df9223ad97e
dietbrand <dietbrand.tollenaere@gmail.com>
2023-12-29 09:15:43 +0100

    Added client side attendance

--- reactivities-client/src/features/activities/details/ActivityDetailedHeader.tsx
+++ reactivities-client/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -1,8 +1,9 @@
 import { observer } from 'mobx-react-lite';
-import { Button, Header, Item, Segment, Image } from 'semantic-ui-react';
+import { Button, Header, Item, Segment, Image, Label } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import { Link } from 'react-router-dom';
 import { format } from 'date-fns';
+import { useStore } from '../../../app/stores/store';
 
 const activityImageStyle = {
   filter: 'brightness(30%)',
@@ -22,9 +23,20 @@ interface Props {
 }
 
 const ActivityDetailedHeader = ({ activity }: Props) => {
+  const {
+    activityStore: { updateAttendance, loading, cancelActivityToggle },
+  } = useStore();
   return (
     <Segment.Group>
       <Segment basic attached='top' style={{ padding: '0' }}>
+        {activity.isCancelled && (
+          <Label
+            style={{ position: 'absolute', zIndex: 100, left: -14, top: 20 }}
+            ribbon
+            color='red'
+            content='Cancelled'
+          />
+        )}
         <Image
           src={`/assets/categoryImages/${activity.category}.jpg`}
           fluid
@@ -41,7 +53,12 @@ const ActivityDetailedHeader = ({ activity }: Props) => {
                 />
                 <p>{format(activity.date!, 'dd MMM yyyy')}</p>
                 <p>
-                  Hosted by <strong>Bob</strong>
+                  Hosted by{' '}
+                  <strong>
+                    <Link to={`/profiles/${activity.host?.username}`}>
+                      {activity.host?.displayName}
+                    </Link>
+                  </strong>
                 </p>
               </Item.Content>
             </Item>
@@ -49,16 +66,42 @@ const ActivityDetailedHeader = ({ activity }: Props) => {
         </Segment>
       </Segment>
       <Segment clearing attached='bottom'>
-        <Button color='teal'>Join Activity</Button>
-        <Button>Cancel attendance</Button>
-        <Button
-          color='orange'
-          floated='right'
-          as={Link}
-          to={`/manage/${activity.id}`}
-        >
-          Manage Event
-        </Button>
+        {activity.isHost ? (
+          <>
+            <Button
+              color={activity.isCancelled ? 'green' : 'red'}
+              floated='left'
+              basic
+              content={
+                activity.isCancelled ? 'Reactivate Activity' : 'Cancel Activity'
+              }
+              onClick={cancelActivityToggle}
+              loading={loading}
+            />
+            <Button
+              disabled={activity.isCancelled}
+              color='orange'
+              floated='right'
+              as={Link}
+              to={`/manage/${activity.id}`}
+            >
+              Manage Event
+            </Button>
+          </>
+        ) : activity.isGoing ? (
+          <Button loading={loading} onClick={updateAttendance}>
+            Cancel attendance
+          </Button>
+        ) : (
+          <Button
+            disabled={activity.isCancelled}
+            loading={loading}
+            onClick={updateAttendance}
+            color='teal'
+          >
+            Join Activity
+          </Button>
+        )}
       </Segment>
     </Segment.Group>
   );
--- reactivities-client/src/features/activities/form/ActivityForm.tsx
+++ reactivities-client/src/features/activities/form/ActivityForm.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import { Activity } from '../../../app/models/activity';
+import { ActivityFormValues } from '../../../app/models/activity';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
@@ -16,25 +16,14 @@ import { v4 as uuid } from 'uuid';
 
 const ActivityForm = () => {
   const { activityStore } = useStore();
-  const {
-    loading,
-    loadActivity,
-    loadingInitial,
-    createActivity,
-    updateActivity,
-  } = activityStore;
+  const { loadActivity, loadingInitial, createActivity, updateActivity } =
+    activityStore;
   const navigate = useNavigate();
   const { id } = useParams();
 
-  const [activity, setActivity] = useState<Activity>({
-    id: '',
-    title: '',
-    category: '',
-    description: '',
-    date: null,
-    city: '',
-    venue: '',
-  });
+  const [activity, setActivity] = useState<ActivityFormValues>(
+    new ActivityFormValues()
+  );
 
   const validationSchema = Yup.object({
     title: Yup.string().required('The activity title is required'),
@@ -46,14 +35,20 @@ const ActivityForm = () => {
   });
 
   useEffect(() => {
-    if (id) loadActivity(id).then(activity => setActivity(activity!));
+    if (id)
+      loadActivity(id).then(activity =>
+        setActivity(new ActivityFormValues(activity))
+      );
   }, [id, loadActivity]);
 
-  const handleFormSubmit = (activity: Activity) => {
+  const handleFormSubmit = (activity: ActivityFormValues) => {
     if (!activity.id) {
-      activity.id = uuid();
-      createActivity(activity).then(() =>
-        navigate(`/activities/${activity.id}`)
+      const newActivity = {
+        ...activity,
+        id: uuid(),
+      };
+      createActivity(newActivity).then(() =>
+        navigate(`/activities/${newActivity.id}`)
       );
     } else {
       updateActivity(activity).then(() =>
@@ -98,7 +93,7 @@ const ActivityForm = () => {
               positive
               type='submit'
               content='Submit'
-              loading={loading}
+              loading={isSubmitting}
             />
             <Button
               as={Link}
