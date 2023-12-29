3cdace5959e3add650a3a5b31c1072591dc94c51
dietbrand <dietbrand.tollenaere@gmail.com>
2023-12-29 14:13:18 +0100

    Added API Image upload

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -14,10 +14,15 @@ namespace Application.Core
             .ForMember(d => d.HostUsername, options => options
             .MapFrom(s => s.Attendees
             .FirstOrDefault(x => x.IsHost).AppUser.UserName));
-            CreateMap<ActivityAttendee, Profiles.Profile>()
+
+            CreateMap<ActivityAttendee, AttendeeDto>()
             .ForMember(d => d.DisplayName, options => options.MapFrom(s => s.AppUser.DisplayName))
             .ForMember(d => d.Username, options => options.MapFrom(s => s.AppUser.UserName))
-            .ForMember(d => d.Bio, options => options.MapFrom(s => s.AppUser.Bio));
+            .ForMember(d => d.Bio, options => options.MapFrom(s => s.AppUser.Bio))
+            .ForMember(d => d.Image, options => options.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
+
+            CreateMap<AppUser, Profiles.Profile>()
+            .ForMember(d => d.Image, options => options.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url));
         }
     }
 }
