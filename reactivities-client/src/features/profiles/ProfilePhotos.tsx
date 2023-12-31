cbb7f4c6330dddbc8d5c10fd10d15630e721bc3d
dietbrand <dietbrand.tollenaere@gmail.com>
2023-12-31 14:39:14 +0100

    Client image upload

---
+++ reactivities-client/src/features/profiles/ProfilePhotos.tsx
@@ -0,0 +1,102 @@
+import { observer } from 'mobx-react-lite';
+import { Button, Card, Grid, Header, Image, Tab } from 'semantic-ui-react';
+import { Photo, Profile } from '../../app/models/profile';
+import { useStore } from '../../app/stores/store';
+import { SyntheticEvent, useState } from 'react';
+import PhotoUploadWidget from '../../app/common/imageUpload/PhotoUploadWidget';
+
+interface Props {
+  profile: Profile;
+}
+
+const ProfilePhotos = ({ profile }: Props) => {
+  const {
+    profileStore: {
+      isCurrentUser,
+      uploadPhoto,
+      uploading,
+      loading,
+      setMainPhoto,
+      deletePhoto,
+    },
+  } = useStore();
+  const [addPhotoMode, setAddPhotoModo] = useState(false);
+  const [target, setTarget] = useState('');
+
+  const handlePhotoUpload = (file: Blob) => {
+    uploadPhoto(file).then(() => setAddPhotoModo(false));
+  };
+
+  const handleSetMainPhoto = (
+    photo: Photo,
+    e: SyntheticEvent<HTMLButtonElement>
+  ) => {
+    setTarget(e.currentTarget.name);
+    setMainPhoto(photo);
+  };
+
+  const handleDeletePhoto = (
+    photo: Photo,
+    e: SyntheticEvent<HTMLButtonElement>
+  ) => {
+    setTarget(e.currentTarget.name);
+    deletePhoto(photo);
+  };
+
+  return (
+    <Tab.Pane>
+      <Grid>
+        <Grid.Column width={16}>
+          <Header icon='image' content='Photos' />
+          {isCurrentUser && (
+            <Button
+              floated='right'
+              basic
+              content={addPhotoMode ? 'Cancel' : 'Add Photos'}
+              onClick={() => setAddPhotoModo(!addPhotoMode)}
+            />
+          )}
+        </Grid.Column>
+        <Grid.Column width={16}>
+          {addPhotoMode ? (
+            <PhotoUploadWidget
+              uploadPhoto={handlePhotoUpload}
+              loading={uploading}
+            />
+          ) : (
+            <Card.Group itemsPerRow={5}>
+              {profile.photos?.map(photo => (
+                <Card key={photo.id}>
+                  <Image src={photo.url} />
+                  {isCurrentUser && (
+                    <Button.Group fluid widths={2}>
+                      <Button
+                        basic
+                        color='green'
+                        content='Main'
+                        name={'main' + photo.id}
+                        disabled={photo.isMain}
+                        loading={target === 'main' + photo.id && loading}
+                        onClick={e => handleSetMainPhoto(photo, e)}
+                      />
+                      <Button
+                        basic
+                        color='red'
+                        icon='trash'
+                        loading={target === photo.id && loading}
+                        onClick={e => handleDeletePhoto(photo, e)}
+                        disabled={photo.isMain}
+                        name={photo.id}
+                      />
+                    </Button.Group>
+                  )}
+                </Card>
+              ))}
+            </Card.Group>
+          )}
+        </Grid.Column>
+      </Grid>
+    </Tab.Pane>
+  );
+};
+export default observer(ProfilePhotos);
