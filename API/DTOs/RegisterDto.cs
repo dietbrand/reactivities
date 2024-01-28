e3a1b3b9729ecc29d25fa43926ba4325bb9b80f1
dietbrand <dietbrand.tollenaere@gmail.com>
2024-01-28 19:09:07 +0100

    Increased password length and changed error message

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -2,17 +2,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs
 {
-    public class RegisterDto
-    {
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
-        [Required]
-        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be complex")]
-        public string Password { get; set; }
-        [Required]
-        public string DisplayName { get; set; }
-        [Required]
-        public string Username { get; set; }
-    }
+  public class RegisterDto
+  {
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}$", ErrorMessage = "Password must be between 8 and 16 characters and use lowercase, uppercase and numbers.")]
+    public string Password { get; set; }
+    [Required]
+    public string DisplayName { get; set; }
+    [Required]
+    public string Username { get; set; }
+  }
 }
